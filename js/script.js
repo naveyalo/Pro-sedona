d80f015fb5eb7f66bdb163289e396038d06d078d
naveyalo <naveyalo@gmail.com>
2019-01-30 17:59:53 +0300

    Pro-sedona

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function() {
+    var menuLink = $('.menu-link');
+    var menu = $('menu');
+    var close = $('.close-btn_1');
+    var navLink = $('li a');
+
+
+    menuLink.click(function() {
+        menu.toggleClass('active-menu');
+    });
+    close.click(function() {
+        menu.toggleClass('active-menu');
+    });
+
+    navLink.on('click', function(event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        var top = $(target).offset().top;
+        $('html,body').animate({scrollTop: top}, 500)
+    });
+});
+
+
+
+
